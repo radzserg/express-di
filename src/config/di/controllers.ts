6f443c73004b69ff83bc421512441e2e5f8954d8
Sergey Radzishevskyi <radzserg@gmail.com>
2021-10-12 19:51:04 +0300

    fix in controllers config

--- src/config/di/controllers.ts
+++ src/config/di/controllers.ts
@@ -1,7 +1,11 @@
 import UsersController from "../../routes/UsersController";
 import { object, use } from "rsdi";
 import { UsersRepository } from "../../lib/UsersRepository";
+import { AppLogger } from "../../lib/Logger";
 
 export const controllers = {
-  [UsersController.name]: object(UsersController).construct(use(UsersRepository))
-}
+  [UsersController.name]: object(UsersController).construct(
+    use(UsersRepository),
+    use(AppLogger)
+  ),
+};
